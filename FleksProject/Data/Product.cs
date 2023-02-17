de47bbc4b0c772e0ef336d1afee1a30ad41375ca
Denis <denis.tanev@abv.bg>
2023-02-17 10:14:00 +0200

    Classes

---
+++ FleksProject/Data/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace FleksProject.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int CategoryId { get; set; }
+        public string Size { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public int Application { get; set; }
+        //public Typea Application 
+        public int SportId { get; set; }
+        public Sport Sport { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        DateTime DateRegister { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
